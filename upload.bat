b0c3ac7534dd0a3c738cf159c557a9cff6da7234
QuantumNecro <QuantumNecro@users.noreply.github.com>
2022-07-18 21:31:59 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
-git config --local user.name "ArieAlchemieLich"
-git config --local user.email "ArieAlchemieLich@users.noreply.github.com"
-git remote set-url origin git@AAL:ArieAlchemieLich/Knowledge.git
+git config --local user.name "QuantumNecro"
+git config --local user.email "QuantumNecro@users.noreply.github.com"
+git remote set-url origin git@AAL:QuantumNecro/Knowledge.git
 
 git add .
 git commit -m "upload"
