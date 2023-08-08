4fb29f9c17bc14e5419df6769b7b82f59c331241
LatticeMage <LatticeMage@users.noreply.github.com>
2023-08-08 16:15:53 +0800

    upload

---
+++ upload.bat
@@ -0,0 +1,8 @@
+git config --local user.name "LatticeMage"
+git config --local user.email "LatticeMage@users.noreply.github.com"
+git remote set-url origin git@LM:LatticeMage/Knowledge.git
+
+git pull
+git add .
+git commit -m "upload"
+git push
--- upload.ps1
+++ upload.ps1
@@ -1,13 +1,4 @@
-git config --local user.name "LatticeMage"
-git config --local user.email "LatticeMage@users.noreply.github.com"
-git remote set-url origin git@LM:LatticeMage/Knowledge.git
-
-git pull
-git add .
-git commit -m "upload"
-git push
-
-
+./upload.bat
 cd ../LatticeMage.github.io/
 ./upload.ps1
 cd ../Knowledge
